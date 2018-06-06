81467e4918ccfd9e2981466c036a6a8fc37b227c
Anil Bunkar <anilkumar4392@gmail.com>
2018-06-06 16:34:37 +0530

    Initial Commit

---
+++ target1/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  target1
+//
+//  Created by Anil Bunkar on 06/06/18.
+//  Copyright © 2018 Adobe Systems. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ target1/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  target1
+//
+//  Created by Anil Bunkar on 06/06/18.
+//  Copyright © 2018 Adobe Systems. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
